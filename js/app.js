04859df034a4f832eca52952a57d2c097d4072ea
Sconix <l.SungJinVVoo.l@gmail.com>
2023-12-10 08:04:05 +0500

    add 3d + rain
    Крч тут добавлена мышка на первом экране, и дождик

--- js/app.js
+++ js/app.js
@@ -378,9 +378,14 @@
     window.addEventListener(`scroll`, (e => {
         document.body.style.cssText += `--scrollTop: ${window.scrollY}px`;
     }));
+    document.addEventListener(`mousemove`, (e => {
+        Object.assign(document.documentElement, {
+            style: `\n\t\t--move-x: ${(e.clientX - window.innerWidth / 2) * -7e-4}deg;\n\t\t--move-y: ${(e.clientY - window.innerHeight / 2) * -7e-4}deg;\n\t\t`
+        });
+    }));
     const lenis = new l({
-        duration: 2.1,
-        easing: t => Math.min(1, 1.021 - Math.pow(2, -10 * t))
+        duration: 1.8,
+        easing: t => Math.min(1, 1.03 - Math.pow(2, -6 * t))
     });
     lenis.on("scroll", (e => {
         console.log(e);
@@ -390,6 +395,48 @@
         requestAnimationFrame(raf);
     }
     requestAnimationFrame(raf);
+    let canvas = document.getElementsByClassName("rain")[0];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let c = canvas.getContext("2d");
+    function randomNum(max, min) {
+        return Math.floor(Math.random() * max) + min;
+    }
+    function RainDrops(x, y, endy, velocity, opacity) {
+        this.x = x;
+        this.y = y;
+        this.endy = endy;
+        this.velocity = velocity;
+        this.opacity = opacity;
+        this.draw = function() {
+            c.beginPath();
+            c.moveTo(this.x, this.y);
+            c.lineTo(this.x, this.y - this.endy);
+            c.lineWidth = 1;
+            c.strokeStyle = "rgba(255, 255, 255, " + this.opacity + ")";
+            c.stroke();
+        };
+        this.update = function() {
+            let rainEnd = window.innerHeight + 100;
+            if (this.y >= rainEnd) this.y = this.endy - 100; else this.y = this.y + this.velocity;
+            this.draw();
+        };
+    }
+    let rainArray = [];
+    for (let i = 0; i < 140; i++) {
+        let rainXLocation = Math.floor(Math.random() * window.innerWidth) + 1;
+        let rainYLocation = Math.random() * -500;
+        let randomRainHeight = randomNum(10, 2);
+        let randomSpeed = randomNum(20, .2);
+        let randomOpacity = Math.random() * .55;
+        rainArray.push(new RainDrops(rainXLocation, rainYLocation, randomRainHeight, randomSpeed, randomOpacity));
+    }
+    function animateRain() {
+        requestAnimationFrame(animateRain);
+        c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        for (let i = 0; i < rainArray.length; i++) rainArray[i].update();
+    }
+    animateRain();
     window["FLS"] = true;
     isWebp();
     //!===================================
